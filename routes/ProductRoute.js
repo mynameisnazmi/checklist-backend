9b961d8044bdad80056e71c8168c63b68ab45379
Sazza540 <athbadanir@gmail.com>
2023-01-10 11:10:15 +0700

    sss

--- routes/ProductRoute.js
+++ routes/ProductRoute.js
@@ -1,15 +1,15 @@
 import express from "express";
 import {
-  getProducts,
+  getUser,
   // getProductById,
   // saveProduct,
   // updateProduct,
   // deleteProduct,
-} from "../controllers/ProductController.js";
+} from "../controllers/users";
 
 const router = express.Router();
 
-router.get("/users", getProducts);
+router.get("/users", getUser);
 //router.get("/products/:id", getProductById);
 // router.post("/products", saveProduct);
 // router.patch("/products/:id", updateProduct);
