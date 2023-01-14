d8b5240ef40f9f590b39b135bb7e2755c28c60ef
Nazmi <117058996+mynameisnazmi@users.noreply.github.com>
2023-01-14 15:09:51 +0700

    much changes

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,7 @@ const Users = require("../models/UserModel");
 //import path from "path";
 //import fs from "fs";
 
-const getUsers = async (req, res) => {
+const getAlluser = async (req, res) => {
   try {
     const response = await Users.findAll();
     res.json(response);
@@ -11,28 +11,43 @@ const getUsers = async (req, res) => {
   }
 };
 
+const addUser = async (req, res) => {
+  try {
+    const { nik, name, password, level, department } = req.body;
+    const addUser = await Users.create({
+      nik: nik,
+      name: name,
+      password: password,
+      level: level,
+      department: department,
+    });
+
+    res.send({
+      Result: "Sukses",
+    });
+  } catch (error) {
+    console.log(error.message);
+  }
+};
+
 const Auth = async (req, res) => {
   const { nik, password } = req.body;
-
-  res.send({
-    test: "ini backend 2",
-    nik,
-    password,
-  });
-  // try {
-  //   const response = await Users.findOne({
-  //     where: {
-  //       nik: 14077,
-  //     },
-  //   });
-  //   res.json(response);
-  // } catch (error) {
-  //   console.log(error.message);
-  // }
+  try {
+    const response = await Users.findOne({
+      where: {
+        nik: nik,
+        password: password,
+      },
+    });
+    res.json(response);
+  } catch (error) {
+    console.log(error.message);
+  }
 };
 
 // Export of all methods as object
 module.exports = {
-  getUsers,
+  getAlluser,
   Auth,
+  addUser,
 };
--- routes/UsersRoute.js
+++ routes/UsersRoute.js
@@ -4,8 +4,9 @@ const UsersController = require("../controllers/UsersController");
 // Initialization
 const router = Router();
 
-router.get("/", UsersController.getUsers);
+router.get("/", UsersController.getAlluser);
 router.post("/", UsersController.Auth);
+router.post("/register", UsersController.addUser);
 //router.get("/products/:id", getProductById);
 // router.post("/products", saveProduct);
 // router.patch("/products/:id", updateProduct);
